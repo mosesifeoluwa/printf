1811213bb6fc2accc776963028c22e91f055be37
Praphil <prapetchi@gmail.com>
2023-04-25 13:17:29 -0700

    update main.c

--- main.c
+++ main.c
@@ -30,7 +30,7 @@ int main(void)
 	printf("Unsigned hexadecimal:[%x, %X]\n", ui, ui);
 	_printf("Character:[%c]\n", 'H');
 	printf("Character:[%c]\n", 'H');
-	_printf("String:[%s]\n", "I am a string !");
+	printf("String:[%s]\n", "I am a string !");
 	printf("String:[%s]\n", "I am a string !");
 	_printf("Address:[%p]\n", addr);
 	printf("Address:[%p]\n", addr);
